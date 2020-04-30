77d471d3eefce207e72059c79cd4be68cac0d220
Faris Aziz <farisaziz12@gmail.com>
2020-05-01 00:11:32 +0200

    leaderboard fully working

--- src/Components/LeaderboardStat.js
+++ src/Components/LeaderboardStat.js
@@ -2,9 +2,16 @@ import React from 'react'
 import './LeaderboardStat.css'
 
 export default function LeaderboardStat(props) {
+
+    const { user, attendances } = props.stat
+
+    const caloriesBurned  = 500 * attendances
+
     return (
-        <div className='leaderboard-stat'>
-            
-        </div>
+        <tr className='leaderboard-stat' style={props.highestNoOfAttendances === attendances? {color: "white", backgroundColor: "green"} : null}>
+            <td>{user}</td>
+            <td>{attendances}</td>
+            <td>~{caloriesBurned} Cal.</td>
+        </tr>
     )
 }
